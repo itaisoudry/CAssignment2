03fa770ee8b658bf47e3a64e70be61572d228d53
itaisoudry <itaisoudry@mail.tau.ac.il>
2017-05-15 02:48:42 -0700

    progress

--- OSAssignment3/dispatcher.c
+++ OSAssignment3/dispatcher.c
@@ -13,102 +13,137 @@
 #include <unistd.h>
 
 #define MAX_LENGTH 1025
+#define COUNTER_PATH "./counter.c"
+#define PIPE_NAME "/tmp/counter_"
 
 void exitMsg(int exitCode, char* message) {
-  printf("%s\n", message);
-  exit(exitCode);
+	printf("%s\n", message);
+	exit(exitCode);
 }
 
 void signalHandler(int signum, siginfo_t* info, void* ptr) {
-  printf("Signal sent from process %lu\n", (unsigned long)info->si_pid);
+	printf("Signal sent from process %lu\n", (unsigned long) info->si_pid);
 }
 
-int getNumberOfProcesses(ssize_t fileSize) {
-  int numOfCounterProcesses = 0;
-  int pageSize = getpagesize();
-  int chunkSize = pageSize;
+pid_t forkCounter(char charToCount, char* filePath, size_t offSet, size_t size) {
+	pid_t pid;
+	char* args[5];
 
-  if (pageSize <= 0) {  // shouldn't happen, but just in case.
-    exitMsg(EXIT_FAILURE, "Error: Cannot get page size\n");
-  }
+	// read current counter result from the pipe
+	if ((pid = fork()) == -1) {
+		printf("Fork failed. %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
 
-  // compute the number of counter processes needed
-  if (fileSize < 2 * pageSize) {
-    numOfCounterProcesses = 1;
-  } else {
-    // calculate chunk size
-    while ((fileSize / chunkSize) > 16) {
-      chunkSize += pageSize;
-    }
+	if (pid == 0) { //child
+		strcpy(args[0], COUNTER_PATH);
+		strcpy(args[1], charToCount);
+		strcpy(args[2], filePath);
+		sscanf(args[3], "%zu", offSet);
+		sscanf(args[4], "%zu", size);
 
-    numOfCounterProcesses = fileSize / chunkSize;
-  }
+		execv(COUNTER_PATH, args);
+	}
 
-  // TODO - delete
-  printf("Number of processes %d\n", numOfCounterProcesses);
-  printf("Chunk size: %d\n", chunkSize);
-
-  return numOfCounterProcesses;
+	return pid;
 }
 
+int getNumberOfProcesses(size_t fileSize) {
+	int numOfCounterProcesses = 0;
+	int pageSize = getpagesize();
+	int chunkSize = pageSize;
+
+	if (pageSize <= 0) {  // shouldn't happen, but just in case.
+		exitMsg(EXIT_FAILURE, "Error: Cannot get page size\n");
+	}
+
+// compute the number of counter processes needed
+	if (fileSize < 2 * pageSize) {
+		numOfCounterProcesses = 1;
+	} else {
+		// calculate chunk size
+		while ((fileSize / chunkSize) > 16) {
+			chunkSize += pageSize;
+		}
+
+		numOfCounterProcesses = fileSize / chunkSize;
+	}
+
+// TODO - delete
+	printf("Number of processes %d\n", numOfCounterProcesses);
+	printf("Chunk size: %d\n", chunkSize);
+
+	return numOfCounterProcesses;
+}
 
 int main(int argc, char** argv) {
-  char charToCount;
-  char filePath[MAX_LENGTH];
-  char errorMsg[MAX_LENGTH];
-  int totalCharCount = 0;
-  int pageSize = 0;
-  int numOfCounterProcesses = 0;
-  int pipeFd[2];
-  ssize_t chuckSize = 0;
-  struct stat status;
-  struct sigaction new_action;
+	char charToCount;
+	char filePath[MAX_LENGTH];
+	char errorMsg[MAX_LENGTH];
+	int totalCharCount = 0;
+	int pageSize = 0;
+	int numOfCounterProcesses = 0;
+	int pipeFd[2];
+	size_t chuckSize = 0;
+	pid_t pid;
+	struct stat status;
+	struct sigaction new_action;
 
-  if (argc != 3) {
-    exitMsg(EXIT_FAILURE, "Error: Invalid arguments");
-  }
+	if (argc != 3) {
+		exitMsg(EXIT_FAILURE, "Error: Invalid arguments");
+	}
 
-  if (strlen(argv[1]) != 1 || !isalpha(argv[1][0])) {
-    exitMsg(EXIT_FAILURE, "Error: Input should be exactly one character");
-  }
+	if (strlen(argv[1]) != 1 || !isalpha(argv[1][0])) {
+		exitMsg(EXIT_FAILURE, "Error: Input should be exactly one character");
+	}
 
-  charToCount = argv[1][0];
-  strcpy(filePath, argv[2]);
+	charToCount = argv[1][0];
+	strcpy(filePath, argv[2]);
 
-  // open file
-  int fd = open(filePath, O_RDONLY);
-  if (fd < 0) {
-    sscanf(errorMsg, "Error: Cannot move file cursor: %s", strerror(errno));
-    exitMsg(EXIT_FAILURE, errorMsg);
-  }
+// open file
+	int fd = open(filePath, O_RDONLY);
+	if (fd < 0) {
+		sscanf(errorMsg, "Error: Cannot move file cursor: %s", strerror(errno));
+		exitMsg(EXIT_FAILURE, errorMsg);
+	}
 
-  // get stats for file size
-  if (fstat(fd, &status) < 0) {
-    sscanf(errorMsg, "Error: Cannot get file stats: %s", strerror(errno));
-    exitMsg(EXIT_FAILURE, errorMsg);
-  }
+// get stats for file size
+	if (fstat(fd, &status) < 0) {
+		sscanf(errorMsg, "Error: Cannot get file stats: %s", strerror(errno));
+		exitMsg(EXIT_FAILURE, errorMsg);
+	}
 
-  numOfCounterProcesses = getNumberOfProcesses(status.st_size);
+	if (status.st_size <= 0) {
+		exitMsg(EXIT_FAILURE, "Error: file is empty\n");
 
-  memset(&new_action, 0, sizeof(new_action));
-  new_action.sa_sigaction = signalHandler;
-  new_action.sa_flags = SA_SIGINFO;
+	}
+	numOfCounterProcesses = getNumberOfProcesses(status.st_size);
 
-  if ((sigaction(SIGUSR1, &new_action, NULL)) != 0) {
-    printf("Signal handle registration failed. %s\n", strerror(errno));
-    return -1;
-  }
+	memset(&new_action, 0, sizeof(new_action));
+	new_action.sa_sigaction = signalHandler;
+	new_action.sa_flags = SA_SIGINFO;
 
-  // for each proccess until all of them are finished
-  for (int i = 0; i < numOfCounterProcesses; i++) {
-    // open named pipe file called “/tmp/counter_PID” for reading
-	pipe(pipeFd);
-    // read current counter result from the pipe
-    // aggregate results to a global data structure
-  }
+	if ((sigaction(SIGUSR1, &new_action, NULL)) != 0) {
+		printf("Signal handle registration failed. %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
 
-  printf("The character %c appears %d times in the file\n", charToCount,
-         totalCharCount);
+// for each process until all of them are finished
+	for (int i = 0; i < numOfCounterProcesses; i++) {
+		// open named pipe file called “/tmp/counter_PID” for reading
+		if (pipe(pipeFd) == -1) {
+			printf("Signal handle registration failed. %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
 
-  return 0;
+		pid = forkCounter(charToCount,filePath,0,0);
+
+		// aggregate results to a global data structure
+	}
+
+	wait(NULL);
+
+	printf("The character %c appears %d times in the file\n", charToCount, totalCharCount);
+
+	return 0;
 }
